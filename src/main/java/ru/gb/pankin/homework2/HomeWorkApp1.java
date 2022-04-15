183985074642552205621ccf443c69a8529495db
gerik <gpankin@gmail.com>
2022-04-15 20:35:57 +0300

    All tasks done

--- src/main/java/ru/gb/pankin/homework2/HomeWorkApp1.java
+++ src/main/java/ru/gb/pankin/homework2/HomeWorkApp1.java
@@ -1,4 +1,20 @@
 package ru.gb.pankin.homework2;
 
 public class HomeWorkApp1 {
+    public static void main(String[] args) {
+        within10and20(11, 6);
+    }
+    public static boolean within10and20(int x1, int x2) {
+        boolean a;
+        int sum = x1 + x2;
+        if (sum >= 10 && sum <= 20) {
+            a = true;}
+        else {
+            a = false;}
+        System.out.println(a);
+        return a;
+
+    }
+
+
 }
