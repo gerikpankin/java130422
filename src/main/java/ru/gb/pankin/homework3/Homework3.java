e07169f995979f89757c9e83914343331e0f0c92
gerik <gpankin@gmail.com>
2022-04-19 19:39:17 +0300

    4th task done

--- src/main/java/ru/gb/pankin/homework3/Homework3.java
+++ src/main/java/ru/gb/pankin/homework3/Homework3.java
@@ -1,34 +1,39 @@
 package ru.gb.pankin.homework3;
 
+import java.util.Arrays;
+
 public class Homework3 {
     public static void main(String[] args) {
         invertArray();
         fillArray();
         changeArray();
+        fillDiagonal();
     }
+
     public static void invertArray() {
-        int[] arr = { 1, 0, 1, 0, 0, 1 };
+        int[] arr = {1, 0, 1, 0, 0, 1};
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0){
+            if (arr[i] != 0) {
                 arr[i] = 0;
-            }
-            else {
+            } else {
                 arr[i] = 1;
             }
             System.out.println(arr[i]);
+        }
     }
-}
+
     public static void fillArray() {
         int[] arr = new int[100];
         arr[0] = 1;
         System.out.println(arr[0]);
         for (int i = 1; i < arr.length; i++) {
-            arr[i] = arr[i-1] + 1;
+            arr[i] = arr[i - 1] + 1;
             System.out.println(arr[i]);
         }
     }
+
     public static void changeArray() {
-        int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) {
                 arr[i] = arr[i] * 2;
@@ -37,8 +42,18 @@ public class Homework3 {
         }
     }
 
-
+    public static void fillDiagonal() {
+        int[][] matrix = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            matrix[i][i] = 1;
+            matrix[i][4 - i - 1] = 1;
+        }
+        System.out.println(Arrays.deepToString(matrix));
+        for (int i = 0; i < 4; i++)
+            System.out.println(Arrays.toString(matrix[i]));
+    }
 }
+
 
 
 
