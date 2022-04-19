88e6b84dde4e4989d2238868a8837112605c12ab
gerik <gpankin@gmail.com>
2022-04-19 18:37:47 +0300

    2nd task done

--- src/main/java/ru/gb/pankin/homework3/Homework3.java
+++ src/main/java/ru/gb/pankin/homework3/Homework3.java
@@ -3,6 +3,7 @@ package ru.gb.pankin.homework3;
 public class Homework3 {
     public static void main(String[] args) {
         invertArray();
+        fillArray();
     }
     public static void invertArray() {
         int[] arr = { 1, 0, 1, 0, 0, 1 };
@@ -15,7 +16,19 @@ public class Homework3 {
             }
             System.out.println(arr[i]);
     }
-}}
+}
+    public static void fillArray() {
+        int[] arr = new int[100];
+        arr[0] = 1;
+        System.out.println(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i-1] + 1;
+            System.out.println(arr[i]);
+        }
+    }
+
+
+}
 
 
 
